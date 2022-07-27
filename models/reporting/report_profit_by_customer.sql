a006cd61ac7d66648803cad4e5a723e0e53a699f
yastheel@ikhokha.com <yastheel@ikhokha.com>
2022-07-27 09:37:03 +0000

    Reporting Model Added

---
+++ models/reporting/report_profit_by_customer.sql
@@ -0,0 +1,12 @@
+select
+        customerid,
+        segment,
+        country,
+        sum(orderprofit) as profit
+
+    from {{ ref('stg_orders') }}
+
+        group by
+                customerid,
+                segment,
+                country
